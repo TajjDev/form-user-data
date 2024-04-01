1ed08fdf24bfe52e7f7365414fcad2e1102db18e
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2024-04-01 10:22:13 +0100

    Update Form.jsx

--- src/component/form/Form.jsx
+++ src/component/form/Form.jsx
@@ -2,57 +2,108 @@
 import React, { useState } from 'react';
 import "./Form.css"
 
-export default function Form({ formSubmit }) {
+export default function Form({ formSubmit}) {
   const [formData, setFormData] = useState({
     username: '',
     email: '',
     password: ''
   });
 
-  const Username=()=>{
-    if (formData.username == "") {
-      return <p>Username is required* </p>
+ const HandleSubmit = (e) => {
+  e.preventDefault();
+    console.log(formData);
+    if (formData.username && formData.email && formData.password != "" ) {
+     formSubmit(formData) 
 
-}
-  }
- const Email=()=>{
-  if (formData.email == "") {
-      return <p>Email is required*</p>
-  }
- }
- const Password=()=>{
-  if (formData.password == "") {
-      return <p>Password is required*</p>
-  }
- }
- const HandleSubmit = (event) => {event.preventDefault();
-    if (formData.username && formData.email && formData.password !== "" ) {
-     formSubmit(formData)
-    }
-    
-  };
+    } }
 
- const handleChange = (event) => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData((prevData) => ({
       ...prevData,
       [name] : value,
     }));
   };
+  const auth1 = ()=>{
+    if (formData.username === "") {
+      document.getElementById("onee").innerHTML="",
+      document.getElementById("o").style.borderColor= "transparent"
+    }
+  }
+  const auth2 = ()=>{
+    if (formData.email === "") {
+      document.getElementById("twoo").innerHTML="",
+      document.getElementById("t").style.borderColor= "transparent"
+    }
+  }
+  const auth3 = ()=>{
+    if (formData.password === "") {
+      document.getElementById("threee").innerHTML="",
+      document.getElementById("th").style.borderColor= "transparent"
+    }
+  }
 
+const auth = ()=>{
+    if (formData.username === "") {
+      document.getElementById("onee").innerHTML="Username can't be empty"
+      document.getElementById("onee").style.color="red"
+      document.getElementById("oo").style.borderColor= "red"
+    }
+    else{
+      document.getElementById("onee").innerHTML=""
+      document.getElementById("oo").style.borderColor= "transparent"
+    }
+    if (formData.email === "") {
+      document.getElementById("twoo").innerHTML="Email can't be empty"
+      document.getElementById("twoo").style.color="red"
+      document.getElementById("t").style.borderColor= "red"
+    }
+    else{
+      document.getElementById("twoo").innerHTML=""
+      document.getElementById("t").style.borderColor= "transparent"
+    }
+    if (formData.password === "") {
+      document.getElementById("threee").innerHTML="Password can't be empty"
+      document.getElementById("threee").style.color="red"
+      document.getElementById("th").style.borderColor= "red"
+    }
+    else{
+      document.getElementById("threee").innerHTML=""
+      document.getElementById("th").style.borderColor= "transparent"
+    }
+}
 
 
   return (
     
     <div>
       <form id="container" onSubmit={HandleSubmit}>
-        <input type="text" placeholder="username" name="username" value={formData.username} onChange={handleChange} />
-        <Username/> 
-        <input type="email" placeholder="email" value={formData.email} onChange={handleChange} name="email" />
-        <Email/>
-        <input type="password" placeholder="Enter password" name="password" value={formData.password} onChange={handleChange} />
-        <Password/>
-        <button >Submit Form</button>
+        <input id='oo'
+          type="text"
+          placeholder="username"
+          name="username"
+          value={formData.username}
+          onChange={(e)=>{handleChange(e), auth1()}}
+          
+        />
+        <p id="onee"></p>
+        <input id='t'
+          type="email"
+          placeholder="email"
+          value={formData.email}
+          onChange={(e)=>{handleChange(e), auth2()}}
+          name="email"
+        />
+        <p id="twoo"></p>
+        <input id='th'
+          type="password"
+          placeholder="Enter password"
+          name="password"
+          value={formData.password}
+          onChange={(e)=>{handleChange(e), auth3()}}
+        />
+        <p id="threee"></p>
+        <button onClick={auth}>Submit</button>
       </form>
     </div>
   );
